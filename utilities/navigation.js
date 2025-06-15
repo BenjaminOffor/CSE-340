ece3353c39d4703902725099b507dfba1a142f56
BenjaminOffor <benjaminoffor10@gmail.com>
2025-06-14 19:56:54 -0700

    Fix duplicate getNav and use relative links" git push

--- utilities/navigation.js
+++ utilities/navigation.js
@@ -1,29 +1,13 @@
+// navigation.js
+
 const { getClassifications } = require("../models/inventory-model");
 
 /**
  * Builds the navigation bar HTML using classifications from the DB
  */
 async function getNav() {
-  let data = await getClassifications();
-  let nav = "<ul>";
-  nav += '<li><a href="/" title="Home page">Home</a></li>';
-  data.rows.forEach((row) => {
-    nav += `<li><a href="/inv/type/${row.classification_id}" title="See our inventory of ${row.classification_name} vehicles">${row.classification_name}</a></li>`;
-  });
-  nav += "</ul>";
-  return nav;
-}
-
-module.exports = getNav;
-const pool = require("../database");
-
-/* =============================
-   Build Dynamic Navigation Menu
-============================= */
-async function getNav() {
   try {
-    const data = await pool.query("SELECT * FROM classification ORDER BY classification_name");
-
+    const data = await getClassifications(); // This uses the model, which uses pool internally
     let nav = '<ul class="navigation">';
     nav += '<li><a href="/" title="Home page">Home</a></li>';
 
